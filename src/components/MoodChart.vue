485122806d844ff06e1fbf71926e0b7bb96452e7
Patrick <patrick@chax.at>
2021-04-25 15:45:09 +0200

    added moddchart

---
+++ src/components/MoodChart.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mood-chart">
+    <h5 class="mood-chart__title">{{title}}</h5>
+    <div class="mood-chart__chart-wrapper">
+      <div
+        v-for="emoji of emojisLeft"
+        :key="emoji"
+        class="mood-chart__emoji"
+      >{{emoji}}</div>
+      <span class="mood-chart__icon material-icons">arrow_forward</span>
+      <div
+        v-for="emoji of emojisRight"
+        :key="emoji"
+        class="mood-chart__emoji"
+      >{{emoji}}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: { type: String, required: true },
+    emojisRight: { type: Array, required: true },
+    emojisLeft: { type: Array, required: true }
+  },
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.mood-chart {
+  @include glass-container;
+  height: min-content;
+
+  &__title {
+    margin-bottom: 16px;
+  }
+
+  &__chart-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__icon {
+    margin: 0 var(--content-spacing);
+    font-weight: bold;
+  }
+
+  &__emoji {
+    font-size: 25px;
+  }
+}
+</style>
--- src/views/Graphs.vue
+++ src/views/Graphs.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="graphs">
     <TimeAllocationChart />
+    <div class="graphs__mini-charts">
+      <MoodChart
+        :title="`Freude`"
+        :emojisLeft="[`🙂`]"
+        :emojisRight="[`😃`]"
+      />
+      <MoodChart
+        :title="`Arbeitskraft`"
+        :emojisLeft="[`👨🏻‍💻`]"
+        :emojisRight="[`👨🏻‍💻`,`👨🏻‍💻`,`👨🏻‍💻`]"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TimeAllocationChart from '@/components/TimeAllocationChart.vue';
+import MoodChart from '@/components/MoodChart.vue';
 
 export default defineComponent({
   name: 'Graphs',
   components: {
-    TimeAllocationChart
+    TimeAllocationChart,
+    MoodChart
   },
   setup() {
     return {};
@@ -22,5 +36,24 @@ export default defineComponent({
 .graphs {
   @include vacation-container;
   margin-top: 40px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 8px;
+
+  @include window-medium {
+    grid-template-columns: 1fr min-content;
+  }
+
+  &__mini-charts {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: min-content;
+    grid-gap: 8px;
+
+    @include window-medium {
+      grid-template-columns: min-content;
+      grid-template-rows: min-content min-content;
+    }
+  }
 }
 </style>
